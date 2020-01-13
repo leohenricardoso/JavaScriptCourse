216550a6f7160840bf3408d55626f0c353e42833
Leonardo Henrique <leohenricardoso@gmail.com>
2020-01-12 22:31:00 -0300

    Adicionado novo modo de criar uma function

--- function.js
+++ function.js
@@ -12,8 +12,10 @@ const soma = (a, b) => {
 // Function salva em uma variavel retornando instantaneamente os dados passados como parametro
 const subtracao = (a, b) => a- b;
 
+const imprimir = a => console.log(a);
 
 // Validando resultados
 console.log(imprimirSoma(10, 2));
 console.log(soma(3, 3));
 console.log(subtracao(5, 2));
+imprimir("Teste");
