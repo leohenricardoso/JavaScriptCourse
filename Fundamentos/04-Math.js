e08e20acece639277629927016eab8e5a7c546ca
Leonardo Henrique <leohenricardoso@gmail.com>
2020-01-18 14:41:54 -0300

    Usando o object Math

---
+++ Fundamentos/04-Math.js
@@ -0,0 +1,12 @@
+/*
+=> Usando o object Math para calcular a area de uma circunferencia
+=> Dentro do object Math temos a constante PI que armazena o valor de PI
+*/
+const raio = 5.6
+const area = Math.PI * Math.pow(raio, 2)
+console.log(area.toFixed(2))
+
+//Output:
+/*
+98.52
+*/
