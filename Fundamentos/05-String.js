2aeb76629f91423c2c2e7d65674ba4aad9f42f5f
Leonardo Henrique <leohenricardoso@gmail.com>
2020-01-19 19:26:54 -0300

    Usando template string

--- Fundamentos/05-String.js
+++ Fundamentos/05-String.js
@@ -32,4 +32,38 @@ Job: Developer
 */
 
 //Parte 2:
+/*
+=> Usando Template string para concatenar dados e formatar texto.
+=> Um template string é delimitado por ( ` ) = Acento grave
+*/
 console.log("\nParte 2: ")
+
+const name = 'Rebeca' //Variavel para ser usada no exemplo
+
+const concatenacao = 'Olá, ' + name + '!' // Forma antiquada de concatenar
+
+const template = `Olá ${name}!` // Usando template string, podemos chamar a variavel dentro da delimitação do template (Todos os espaços serão usados => ex: Espaço entre "Olá" e "${name}")
+
+const template2 = `
+    Olá ${name}!
+    Tudo bem?` //Nesse caso ele levará consideração na quebra de linha
+
+console.log('Concatenação: ', concatenacao)
+console.log('Template: ', template)
+console.log('Template2: ', template2)
+
+// => Podemos também chamar funções dentro de um template string
+const up = txt => txt.toUpperCase() //Função arrow, recebe um texto e retorna ele com uppercase
+console.log(`Hey.. ${up('cuidado!')}`) // Usando função dentro do template string
+
+
+//Output:
+/*
+Parte 2:
+Concatenação:  Olá, Rebeca!
+Template:  Olá Rebeca!
+Template2:
+    Olá Rebeca!
+    Tudo bem?
+Hey.. CUIDADO!
+*/
