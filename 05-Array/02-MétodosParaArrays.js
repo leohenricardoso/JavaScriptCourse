018bb1c9bad6acc9499ef1da00d815bbcbba0c83
Leonardo Henrique <leohenricardoso@gmail.com>
2020-01-24 19:46:44 -0300

    Alguns métodos para array

---
+++ 05-Array/02-MétodosParaArrays.js
@@ -0,0 +1,33 @@
+/*
+
+*/
+const jogadores = ['C. Ronaldo', 'Neymar', 'Messi', 'Pelé']
+
+jogadores.pop() //Removes the last element from an array and returns it.
+console.log(jogadores)
+
+jogadores.push('Ronaldinho') //Appends new elements to an array, and returns the new length of the array.
+console.log(jogadores)
+
+jogadores.shift() //Removes the first element from an array and returns it.
+console.log(jogadores)
+
+jogadores.unshift('Kaka') //Adiciona ao inicio
+console.log(jogadores)
+
+jogadores.splice(1, 1, 'Xavi') //Tira do array e coloca outro caso passe em parametro novo dado
+console.log(jogadores)
+
+const players = jogadores.slice(2) // Retorna o array a partir do indice indicado no parametro
+console.log(players)
+
+const players2 = jogadores.slice(0, 3) //Retorna um array do primeiro parametro até o segundo parametro
+console.log(players2)
+
+
+
+
+//Output:
+/*
+
+*/
