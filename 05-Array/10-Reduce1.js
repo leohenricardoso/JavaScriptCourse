47cab257e314304bd0f7a0971134233fa724204c
Leonardo Henrique <leohenricardoso@gmail.com>
2020-01-25 00:36:01 -0300

    Usando reduce 1

---
+++ 05-Array/10-Reduce1.js
@@ -0,0 +1,29 @@
+/*
+=> A função reduce acumula os dados de um determinado array
+=> acumulador = dados já somados anteriormente
+=> atual = dado atual que irá ser acumulado
+=> Se quiser, pode passar após passar o acumulador e o atual, um valor inicial.
+*/
+const alunos = [
+    { nome: 'João', nota: 7.3, bolsista: false },
+    { nome: 'Maria', nota: 9.2, bolsista: true },
+    { nome: 'Pedro', nota: 9.8, bolsista: false },
+    { nome: 'Ana', nota: 8.7, bolsista: true },
+]
+
+console.log(alunos.map(a => a.nota))    // Ver todas as notas de todos os alunos
+
+const resultado = alunos.map(a => a.nota).reduce(function(acumulador, atual) {
+    console.log(acumulador, atual)
+    return acumulador + atual
+}) //Caso quisesse passar um valor inicial para essa acumulação, deveria passar após fechar a function callback
+
+console.log(resultado)
+//Output:
+/*
+[ 7.3, 9.2, 9.8, 8.7 ]
+7.3 9.2
+16.5 9.8
+26.3 8.7
+35
+*/
