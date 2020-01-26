d321d7ec572ec453dbb926a99c06b3742632d68f
Leonardo Henrique <leohenricardoso@gmail.com>
2020-01-25 21:52:47 -0300

    Descrição da aula

--- 06-Node/02-UsandoModulosTerceiros.js
+++ 06-Node/02-UsandoModulosTerceiros.js
@@ -1,5 +1,8 @@
 /*
-
+=> Usamos o comando 'npm i lodash' para instalar o módulo lodash
+=> O módulo é de terceiros
+=> O módulo foi instalado dentro da pasta node_modules\lodash
+=> Nesse arquivos usamos apenas um método desse módulo para testar se está funcionando
 */
 
 const _ = require('lodash')
